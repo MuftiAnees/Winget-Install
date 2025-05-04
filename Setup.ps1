eab9c6651dd8af72826c0fa87b88881bfb324fce
Anees Mufti <89216202+MuftiAnees@users.noreply.github.com>
2025-05-04 17:46:45 +0500

    Arranged Apps
    Arranged Installs according to my preferences

--- Setup.ps1
+++ Setup.ps1
@@ -1,25 +1,25 @@
 Write-Host "Installing commonly used applications..." -ForegroundColor Green
 
 $appsToInstall = @(
-    "Vencord.Vesktop",
-    "Proton.ProtonVPN",
-    "VideoLAN.VLC",
-    "7zip.7zip",
-    "Discord.Discord",
-    "Microsoft.VisualStudioCode",
     "Waterfox.Waterfox",
-    "OBSProject.OBSStudio",
-    "Valve.Steam",
-    "qBittorrent.qBittorrent",
     "Bitwarden.Bitwarden",
-    "Notion.Notion",
     "amir1376.ABDownloadManager",
+    "7zip.7zip",
     "Brave.Brave",
+    "Discord.Discord",
+    "EpicGames.EpicGamesLauncher",
     "Figma.Figma",
     "Figma.FigmaAgent",
     "GitHub.GitHubDesktop",
-    "Spotify.Spotify",
     "Microsoft.VisualStudioCode",
+    "Notion.Notion",
+    "OBSProject.OBSStudio",
+    "Proton.ProtonVPN",
+    "qBittorrent.qBittorrent",
+    "Spotify.Spotify",
+    "Valve.Steam",
+    "Vencord.Vesktop",
+    "VideoLAN.VLC"
     )
 
 foreach ($app in $appsToInstall) {
